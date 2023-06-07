a956f9c9dc51c307cc3be71f3d86bce41d961858
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-06-07 16:38:01 -0400

    Chore: Optimised the duplicate nodes function

--- public/modules/parseFunctions.js
+++ public/modules/parseFunctions.js
@@ -1,42 +1,30 @@
 function createDuplicateNodes(duplicateNode, nodes, links, amounts) {
-    
-    const targetNodes = links.filter(l => l.source.name === duplicateNode.name)             // Get the duplicate target nodes
-        .map(l => l.target);
-    const sourceNodes = links.filter(l => l.target.name === duplicateNode.name)             // Get the duplicate source nodes
-        .map(l => l.source);
+    const targetNodes = links.filter(l => l.source.name === duplicateNode.name).map(l => l.target);
+    const sourceNodes = links.filter(l => l.target.name === duplicateNode.name).map(l => l.source);
 
-    const uniqueSourceNodes = [...new Set([...sourceNodes])];                               // Get the unique source and target nodes
-    const uniqueTargetNodes = [...new Set([...targetNodes])];                               // Get the unique source and target nodes
+    const uniqueSourceNodes = Array.from(new Set(sourceNodes));
+    const uniqueTargetNodes = Array.from(new Set(targetNodes));
 
-    if (uniqueSourceNodes) {
-        if (uniqueSourceNodes.length > 0) {                                                 // If there is only one duplicate add it normally
-            for (const linkedNode of uniqueSourceNodes) {
-                const newNode = { ...duplicateNode };
-                nodes.push(newNode);
-                links.push({ source: linkedNode, target: newNode });
-            }
-            links.filter(l => l.source.name === duplicateNode.name)                         // Push the duplicate links
-                .forEach(l => links.splice(links.indexOf(l), 1));
-        } else if (uniqueSourceNodes[0]) {                                                  // If there is only one duplicate add it normally
-            const newNode = { ...sourceNodes };
-            nodes.push(newNode);
-            amounts[sourceNodes.type] = (amounts[sourceNodes.type] || 0) + 1;
-        }
-    }
-    if (uniqueTargetNodes) {
-        if (uniqueTargetNodes.length > 0) {                                                 // If there are multiple duplicates add them uniquely
-            for (const linkedNode of uniqueTargetNodes) {
-                const newNode = { ...duplicateNode };
-                nodes.push(newNode);
-                links.push({ source: linkedNode, target: newNode });
-            }
-            links.filter(l => l.source.name === duplicateNode.name)                          // Push the duplicate links
-                .forEach(l => links.splice(links.indexOf(l), 1));
-        } else if (uniqueTargetNodes[0]) {                                                   // If there is only one duplicate add it normally
-            const newNode = { ...targetNodes };
-            nodes.push(newNode);
-            amounts[targetNodes.type] = (amounts[targetNodes.type] || 0) + 1;
-        }
+    uniqueSourceNodes.forEach(linkedNode => {
+        const newNode = { ...duplicateNode };
+        nodes.push(newNode);
+        links.push({ source: linkedNode, target: newNode });
+    });
+
+    uniqueTargetNodes.forEach(linkedNode => {
+        const newNode = { ...duplicateNode };
+        nodes.push(newNode);
+        links.push({ source: linkedNode, target: newNode });
+    });
+
+    const duplicateLinks = links.filter(l => l.source.name === duplicateNode.name);
+    duplicateLinks.forEach(l => {
+        links.splice(links.indexOf(l), 1);
+    });
+
+    if (uniqueSourceNodes.length === 1) {
+        const sourceType = uniqueSourceNodes[0].type;
+        amounts[sourceType] = (amounts[sourceType] || 0) + 1;
     }
 }
 
